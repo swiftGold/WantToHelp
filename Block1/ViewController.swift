3967882425f6984cd5a74c9accf8478616a00d4f
Сергей Золотухин <sergejzolotuhin@Sergejs-MacBook-Air.local>
2023-05-19 16:14:46 +0300

    initial commit

---
+++ Block1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Block1
+//
+//  Created by Сергей Золотухин on 19.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
+
